ddd2f964aa7b21f71dc79b00b43862e81f1f54cf
Barış Sokat <barissokat@outlook.com>
2022-01-21 16:15:51 +0300

    Add redux to components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
+import { fetchPostsRequest } from './redux/actions/postsActions'
 
 function App () {
+  const dispatch = useDispatch()
+  const { pending, posts, error } = useSelector(state => state.posts)
+
+  useEffect(() => {
+    dispatch(fetchPostsRequest())
+  }, [])
+
   return (
-    <div className='App' />
+    <div className='App'>
+      <h1>Posts</h1>
+      {pending
+        ? (
+          <div>Loading...</div>
+          )
+        : error
+          ? (
+            <div>Error!</div>
+            )
+          : (
+              posts?.map((post, index) => (
+                <div key={post.id}>
+                  {++index}. {post.title}
+                </div>
+              ))
+            )}
+    </div>
   )
 }
 
